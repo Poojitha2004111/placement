46d3776fa5892ee48b51c17a07bf3c3bf0961bd4
Poojitha2004111 <poojithaprana@gmail.com>
2025-06-06 12:25:40 +0530

    Java

---
+++ ro.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class ro {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int arr[][] = new int[3][3];
+        for (int j = 0; i < Array.length; i++) {
+            arr[i][j] = sc.nextInt();
+        }
+    }
+
+for (int index = 0; index < arr.length; index++) {
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum+arr[i][index];
+
+        }
+        System.out.println(sum);
+        sum = 0;
+        }
+    }
+}
